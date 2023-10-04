226a93d1c6235c271183f31a75e1ef86ed765ecd
MadoXZA <timklo15@gmail.com>
2023-10-04 00:54:02 -0700

    Task Completed

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,22 +14,22 @@ chgrp -R ubuntu /data/
 
 printf %s "server {
     listen 80 default_server;
-        listen [::]:80 default_server;
-	    add_header X-Served-By $hostname;
-	        root   /var/www/html;
-		    index  index.html index.htm;
-		        location /hbnb_static {
-			        alias /data/web_static/current;
-				        index index.html index.htm;
-					    }
-				        location /redirect_me {
-					        return 301 http://github.com/MadoXZA;
-						    }
-					        error_page 404 /404.html;
-						    location /404 {
-						          root /var/www/html;
-							        internal;
-								    }
-						    }" > /etc/nginx/sites-available/default
+    listen [::]:80 default_server;
+    add_header X-Served-By $hostname;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://github.com/MadoXZA;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
-					    service nginx restart
+service nginx restart
